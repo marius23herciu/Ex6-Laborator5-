cfcfa37447da2f7cb3b00aa224f980ac004c3638
marius.herciu23 <marius.herciu23@gmail.com>
2022-06-03 19:17:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex6_Laborator5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 6
+            //Screiti un program care va numara vocalele dintr-un sir de caractere citit de
+            //la tastatura.
+
+            Console.WriteLine("Introduceti un sir de caractere");
+            string sir = Console.ReadLine();
+
+            int vocale = NumerotareVocale(sir);
+            Console.WriteLine($"Sirul introdus are {vocale} vocale.", vocale);
+        }
+        static int NumerotareVocale(string sir)
+        {
+            int rezultat = 0;
+            string vocale = "aeiou";
+            for (int i = 0; i < sir.Length; i++)
+            {
+                if (vocale.Contains(Char.ToLower(sir[i])))
+                {
+                    rezultat++;
+                }
+            }
+
+            return rezultat;
+        }
+    }
+}
